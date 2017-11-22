75ee772a8f19eb21c4e39a6effcc70016b2abce7
Mattia Sicoli <mattia.sicoli@hotmail.it>
2017-11-22 16:19:04 +0100

    Changed return type from void to File in all JZip Class's Methods

--- src/me/mattsay/jzip/Jzip.java
+++ src/me/mattsay/jzip/Jzip.java
@@ -9,44 +9,48 @@ import java.util.zip.ZipOutputStream;
  * Class that compress and decompress files
  *
  * @author Mattia Sicoli
- * @version 1.0
+ * @version 1.1
  */
 public class Jzip {
 
     public static final int BUFFER = 4096;
 
     /**
-     * Decompresses all files from a compressed file
+     * Returns all decompressed files from a compressed file
      * @param outdir the path where the program decompresses the files
      * @param path the path of the file to convert
      * @throws IOException throws if the file does not exist
      */
-    public static void decompressFiles(String outdir , String path) throws IOException {
+    public static File[] decompressFiles(String outdir , String path) throws IOException {
+        File[] file;
+        int i = 0;
         FileInputStream fis = new FileInputStream(path);
         ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
         ZipEntry entry;
+        file = new File[zis.available()];
         while((entry = zis.getNextEntry()) != null){
             if(entry.isDirectory()){
                 new File(outdir, entry.getName()).mkdir();
                 continue;
             }
-            decompressFile(entry, zis, outdir);
-
+            file[i] = decompressFile(entry, zis, outdir);
+            i++;
         }
         zis.closeEntry();
         zis.close();
-
+        return file;
     }
 
     /**
-     * Decompresses the a file
+     * Returns decompressed file
      * @param entry all file's informations
      * @param zis gets data from the compressed file
      * @param outdir path where the program decompress the file
      * @throws IOException throws if the file does not exist
      */
-    private static void decompressFile(ZipEntry entry,ZipInputStream zis, String outdir) throws IOException{
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(outdir, entry.getName())),BUFFER);
+    private static File decompressFile(ZipEntry entry,ZipInputStream zis, String outdir) throws IOException{
+        File file = new File(outdir, entry.getName());
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file),BUFFER);
         byte[] data = new byte[BUFFER];
         int count = -1;
         double progress = 0,changedpercent = 0,percent = 0;
@@ -61,16 +65,18 @@ public class Jzip {
         System.out.println(Character.MIN_VALUE);
         System.out.println(ChatColor.ANSI_CYAN + "Decompressed: \n  File: " + entry.getName() + "\n  Size: " + (entry.getSize()<1024 ? entry.getSize() + " B" : entry.getSize()/1024 + " KB"));
         bos.close();
+        return file;
     }
 
     /**
-     * Compresses all files in a single file
+     * Returns all files compressed into a single file
      * @param files All files to be compressed
      * @param destination the path where the program compress the file
      * @throws IOException throws if the file does not exist
      */
-    public static void compressFiles(File[] files, String destination) throws IOException{
-        FileOutputStream fos = new FileOutputStream(destination);
+    public static File compressFiles(File[] files, String destination) throws IOException{
+        File file = new File(destination);
+        FileOutputStream fos = new FileOutputStream(file);
         ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(fos));
         for(File f : files){
             if(f.isDirectory()){
@@ -81,10 +87,11 @@ public class Jzip {
         }
         zos.close();
         fos.close();
+        return file;
     }
 
     /**
-     * Compresses a file
+     * Compress file
      * @param f File to compress
      * @param zos Writes all data
      * @throws IOException throws if the file does not exist
